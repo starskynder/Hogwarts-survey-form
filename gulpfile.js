4bbbebe1833d54182a6ed291393c53bca0ac8b59
starskynder <24276076+starskynder@users.noreply.github.com>
2018-08-21 11:19:55 +0200

    Add survey's sections

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
-const browserSync = require("browser-sync").create();
 
-gulp.task("sass", () => {
+gulp.task("sass", function() {
   return gulp
     .src("scss/*.scss")
     .pipe(sass().on("error", sass.logError))
@@ -17,13 +17,13 @@ gulp.task("sass", () => {
     .pipe(browserSync.stream());
 });
 
-gulp.task("serve", ["sass"], () => {
+gulp.task("serve", ["sass"], function() {
   browserSync.init({
     server: "./dist"
   });
 
   gulp.watch("scss/*.scss", ["sass"]);
-  gulp.watch("dist/*.html").on("change", browserSync.reload());
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("default", ["serve"]);
